f4f3d69c567b74b0b4a6d404f20ea384186bf4c3
latch-n-load <akifali.it@gmail.com>
2026-09-21 20:56:55 +0200

    Updated program_fpga for persistent xsct session using stdin, stdout

--- pynq-z2/fault_test/scripts/program_fpga.tcl
+++ pynq-z2/fault_test/scripts/program_fpga.tcl
@@ -1,11 +1,8 @@
-# This script receives bitstream as arguments from Python subprocess and programs
-# PYNQ-Z2 board using XSCT
+# This script receives bitstream over stdin from Python, stdout info, 
+# and programs PYNQ-Z2 board using XSCT
 
-# Get bitstream name from the Python subprocess
-set bitstream_file [lindex $argv 0]
-
-# Connect to the physical JTAG hardware 
 connect
+puts "xsct% Connected to hardware server"
 # TODO check if name matches FPGA
 # Example
 # 1  Xilinx HW-USB-II-G 0000185e495201
@@ -16,22 +13,43 @@ connect
 #      6  xc7z020 (idcode: 23727093)      <-- (My XC7Z020!)
 targets -set -filter {name =~ "xc7z020*"}
 
-# Attempt to program the FPGA and catch any hardware/file errors
-if { [catch {fpga -file $bitstream_file} result] } {
-    puts "FPGA_PROGRAM_ERROR: Failed to push bitstream. Reason: $result"
-    disconnect
-    exit 1
+# Signal to Python that initialization is complete
+puts "READY"
+flush stdout
+
+# Enter infinite loop
+while {1} {
+    # Wait for Python to send a bitstream path over stdin
+    set bitstream_file [gets stdin]
+
+    # Allow Python to cleanly close the session
+    if {$bitstream_file == "EXIT"} {
+        break
+    }
+
+    # Ignore empty lines
+    if {$bitstream_file == ""} {
+        continue
+    }
+
+    # Program the FPGA with error stdout
+    if { [catch {fpga -file $bitstream_file} result] }
+        puts "FPGA_PROGRAM_ERROR: Failed to push bitstream. Reason: $result"
+        flush stdout
+        continue
+    }
+
+    # Get configuration status and stdout
+    set is_configured [fpga -state]
+    if { $is_configured != "FPGA is configured" } {
+        puts "FPGA_PROGRAM_ERROR: Bitstream pushed, but FPGA DONE pin did not assert!"
+        flush stdout
+        continue
+    }
+    # Else stdout programming successful
+    puts "PROGRAM_DONE"
+    flush stdout
 }
 
-# Verify configuration status
-# The -state flag returns 1 if configured, 0 if not configured.
-set is_configured [fpga -state]
-if { $is_configured != "FPGA is configured" } {
-    puts "FPGA_PROGRAM_ERROR: Bitstream pushed, but FPGA DONE pin did not assert!"
-    disconnect
-    exit 1
-}
-
-# Exit successfully
 disconnect
 exit 0
